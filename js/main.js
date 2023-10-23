f492e1e9f3011e214c9bfe31f3d07c830a415cb5
MaheshBoopathiAP <727821tuit055@gmail.com>
2023-10-23 08:14:00 +0530

    Gallery

--- js/main.js
+++ js/main.js
@@ -41,3 +41,15 @@ const navLinks = document.querySelectorAll('.nav-link');
     });
 
     
+
+  
+/*Modal Gallery*/
+
+ document.addEventListener("click",function (e){
+  if(e.target.classList.contains("gallery-item")){
+      const src = e.target.getAttribute("src");
+      document.querySelector(".modal-img").src = src;
+      const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+      myModal.show();
+  }
+})
